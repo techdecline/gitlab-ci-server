188b43bf17f95d4ee14ef14593e3217664f01294
techdecline <cornelius.schuchardt@gmail.com>
2021-10-12 10:04:53 +0000

    output added for vm id

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "gitlab_vm_name" {
   value = local.gitlab_vm_name
 }
 
+output "gitlab_vm_id" {
+  value = azurerm_linux_virtual_machine.vm-gitlab.id
+}
+
 output "gitlab_vm_ip_address" {
   value = var.create_public_ip ? azurerm_public_ip.pip-gitlab[0].ip_address : azurerm_network_interface.nic-gitlab.private_ip_address
 }
